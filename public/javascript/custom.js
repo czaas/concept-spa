358c54f9f6aa6951e0ab75d659e818fac61accff
Cameron Zaas <cameron@etnainteractive.com>
2015-05-05 14:13:29 -0700

    Added feature: Can now add people to database

--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
 	
 	// Username link click
 	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+	
+	// Add user on button click
+	$('#btnAddUser').on('click', addUser);
+	
 });
 
 // functions =================================
@@ -64,5 +68,65 @@ function showUserInfo(event){
 	$('#userInfoAge').text(thisUserObject.age);
 	$('#userInfoGender').text(thisUserObject.gender);
 	$('#userInfoLocation').text(thisUserObject.location);
+	
+}
+
+function addUser(event){
+	event.preventDefault();
+	
+	// Basic volidation - increase errorCount var if any fields are blank
+	var errorCount = 0;
+	$('#addUser input').each(function(index, val){
+		if($(this).val() === '') { errorCount++; }
+	});
+	
+	// check the counter to make sure it's at zero
+	if(errorCount === 0){
+		
+		// compile all user info into one object
+		var newUser = {
+			'username': $('#addUser fieldset input#inputUserName').val(),
+			'email': $('#addUser fieldset input#inputUserEmail').val(),
+			'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+			'age': $('#addUser fieldset input#inputUserAge').val(),
+			'location': $('#addUser fieldset input#inputUserLocation').val(),
+			'gender': $('#addUser fieldset input#inputUserGender').val()
+		};
+		
+		// Use ajax to post the object to adduser service
+		$.ajax({
+			type: 'POST',
+			data: newUser,
+			url: '/users/adduser',
+			dataType: 'JSON'
+		}).done(function(response){
+			
+			// check for successful (blank) response
+			if(response.msg === ''){
+				
+				// clear the form inputs
+				$('#addUser fieldset input').val('');
+				
+				// update the table
+				populateTable();
+				
+			} else {
+				
+				// if something goes wrong, alert the error message that our service returned
+				alert('Error: ' + response.msg);
+				
+			}
+			
+			
+		});
+		
+		// else of if(errorCount === 0)
+	} else {
+		
+		// if error count is more that zero, error out
+		alert('Please fill in all fields');
+		return false;
+		
+	}
 	
 }
